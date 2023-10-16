680d42a34135a18392bc21873e18bc6b920bcb0d
Yardley <D.Yardley3767@student.leedsbeckett.ac.uk>
2023-10-16 11:39:10 +0100

    update files

--- Week4DefiningFunctions/04Programs.py
+++ Week4DefiningFunctions/04Programs.py
@@ -87,3 +87,8 @@ if __name__ == "__main__":
     """
 
     # Question 7.
+    # Write a program that reads 6 temperatures (in the same format as before), and
+    # displays the maximum, minimum, and mean of the values.
+    # Hint: You should know there are built-in functions for max and min. If you hunt, you
+    # might also find one for the mean.
+
