d4f580fa41b7361726b652b7089927aabdd445de
DavidYardley19 <d.yardley3767@student.leedsbeckett.ac.uk>
2023-11-12 21:11:32 +0000

    updates

--- Week1IntroductionToProgramming/01Exercises.py
+++ Week1IntroductionToProgramming/01Exercises.py
@@ -2,22 +2,42 @@
 
 if __name__ == "__main__":
 
-    print("Good day. Welcome to the exercises for the first week!\n"
-          "This will be set out as a multiple choice quiz :)\n"
-          "You will only need to output a single letter between A-D per question.\n")
+    print("I decided not to do a quiz in the end. It would be a waste of time to create a basic quiz each week...")
+    print("")
 
-    print("Question 1\n"
-          "What is the name of the programming language that we will be using on this module?\n"
-          "A. Pascal\n"
-          "B. PHP\n"
-          "C. Perl\n"
-          "D. Python\n")
-    answer = input("Enter your answer: ")
-    if answer.capitalize() == "D":
-        print("YES!")
-    else:
-        print("No!")
+    # What is the name ofthe programming language that we will be using on this module... What version of the language are we using
+    print("We are using python... I believe 3.8 and above is just fine.")
 
+    # A computer program takes in some input, performs some processing then what
+    print("input, processing, output")
 
+    # What generation of programming language is machine code?
+    print("1GL is machine code and only machine code... I believe")
 
+    # Which of the following is known as a secondfary generation programming lang: C++ Java Assembly R Python
+    print("2gl is assembly code and only assembly code, I believe?")
+
+    # State one problem associated with writing code in assembly languages
+    print("I believe one issue with writing in assembly, is that it is much harder to debug than more user friendly languages like python")
+
+    # What generational language is python?
+    print("Python is 4gl since it is a scripting language. Other 4gl languages may be specific to database usage.")
+
+    # What is the purpose of a compiler
+    print("It is designed to convert source code into machine code. That being the programming languages code into 1s and 0s")
+
+    # What is meant by REPL
+    print("Read, eval, loop, print")
+
+    # Is it true that python dev always has to take place using interactive mode?
+    print("Python only in interactive mode? Nope, it can be done in a text file :) it just requires the .py extension")
+
+    # What does the term IDE stand for?
+    print("IDE means interactive development environment... nope. *integrated*")
+
+    # What is the main reason why programmers use code libraries.
+    print("They save HELLA time. Think of importing math for example... it contains the sqrt() function")
+
+    # Python supports data science. What other language specifically supports data science.
+    print("R supports data science. I hated R")
 
