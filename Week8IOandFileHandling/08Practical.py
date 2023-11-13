ad670f3b597163522ce53cfb836273586cc0b1cb
Yardley <D.Yardley3767@student.leedsbeckett.ac.uk>
2023-11-13 11:10:39 +0000

    updates

--- Week8IOandFileHandling/08Practical.py
+++ Week8IOandFileHandling/08Practical.py
@@ -18,3 +18,23 @@ if __name__ == "__main__":
 
     # FORMAT SPECIFIERS
 
+    print("The real power from f strings comes from format specifiers"
+          "This is a small string that is included after expressions, prefixed by a : character.")
+
+    import math
+    r = 10
+    print(f"A circle with radius {r} has an area of {math.pi * r * r:.2f}")
+    # Here, .2f implies the decimal points
+
+    # Task - rewrite rectangle code but include format specifier that limits displayed result to 3 dp
+    print(f"The area of a rectangle with a width of {width} and a height of {height} is {width * height:.3f}")
+
+    print("You can also use format specifiers for column width and alignment")
+    name = "Dave"
+    age = 22
+    print(f"{name:>15} - {age:10}")
+    print(f"{name:a^15} - {age:10}")
+    print(f"{name:#<15} - {age:10}")
+
+    # Task: write print statement displaying name and age w width of 20 for each, both alligned right... age to 2dp, fill with $
+    print(f"{name:>20}|{age:>20}")
